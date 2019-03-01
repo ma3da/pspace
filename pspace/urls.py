b315646906e14d69586bf76c3baff5050de3e2b5
lpc <lpc@>
2019-03-01 12:40:38 +0100

    more css + menu button

--- pspace/urls.py
+++ pspace/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.urls import include, path
 from . import views
+from .forms import CustomAuthForm
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/login/', LoginView.as_view(authentication_form=CustomAuthForm), name='login'),
     path('logout', views.logout_view, name='logout'),
     path('tt/', include('timetable.urls')),
     path('admin/', admin.site.urls),
