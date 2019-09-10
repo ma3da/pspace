97b03ee7beb00641fec8a207a639823d37b72be9
lpc <lpintocastaneda@gmail.com>
2019-09-10 21:37:21 +0200

    defi getting real content

--- definition/views.py
+++ definition/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import requests
+import bs4
 
 
 def index(request):
     return render(request, "definition/index.html")
 
 
+def check_input(word):
+    if word == "":
+        return False
+    return True
+
+
 def get_definition_html(word):
-    return word
+    url = f"https://www.cnrtl.fr/definition/{word}"
+    resp = requests.get(url)
+    soup = bs4.BeautifulSoup(resp.content, "html.parser")
+    arts = [div for div in soup("div") if "id" in div.attrs and div["id"].startswith("art")]
+    return str(arts[0])
 
 
 class DefinitionView(APIView):
     def get(self, request, word, format=None):
-        html_content = get_definition_html(word)
+        html_content = get_definition_html(word) if check_input(word) else "Nope"
         return Response({"htmlcontent": html_content})
