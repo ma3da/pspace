716ccb3fbcfa64d70c2ae95f51801a0066957340
lpc <lpintocastaneda@gmail.com>
2019-10-09 08:05:31 +0200

    adding transaction model

--- lbcflba/models.py
+++ lbcflba/models.py
@@ -1,10 +1,11 @@
-import datetime
-
 from django.db import models
+from django.conf import settings
 
 
 class Transaction(models.Model):
-    source = models.CharField(max_length=256)
-    destination = models.CharField(max_length=256)
+    source = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    destination = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=14, decimal_places=2)
+    text = models.TextField()
     time = models.DateTimeField()
-    amount = models.FloatField()
+    status = models.PositiveSmallIntegerField()
