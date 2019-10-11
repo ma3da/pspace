3e23c7b833c76cf46e4d93dc8e65e34608471ab9
lpc <lpintocastaneda@gmail.com>
2019-10-11 16:42:25 +0200

    a recap. js code starting to look bad

--- lbcflba/static/lbcflba/app.js
+++ lbcflba/static/lbcflba/app.js
@@ -43,26 +43,68 @@ Vue.component('daily', {
             } else {
                 color = "Gray";
             }
-            return {color: color};
+            return {color: color, background: "white", width: "10%"};
         },
     },
     props: ["transaction", "users"],
     template: `
-    <table class="main_table" style="border: 1px solid #eeeeee">
+    <table class="main_table" style="border: 2px solid #eeeeee; color: Gray; background: #eeeeee">
         <tr>
-            <td rowspan="2" v-bind:style="getAmountStyle(transaction, users)"> {{ transaction.amount }}</td>
-            <td> {{ getDirectionStr(transaction, users) }} </td>
-            <td colspan="2"> {{ getTimeStr(transaction) }} </td>
+            <td rowspan="2" v-bind:style="getAmountStyle(transaction, users)"> {{ transaction.amount }} </td>
+            <td style="text-align: left"> {{ getDirectionStr(transaction, users) }} </td>
+            <td colspan="2" style="text-align: right"> {{ getTimeStr(transaction) }} </td>
         </tr>
         <tr>
-            <td> {{ transaction.text }} </td>
+            <td style="width: 80%"> {{ transaction.text }} </td>
             <td> </td>
-            <td @click="$emit('showmodify', transaction.id)"> {{ transaction.status }} </td>
+            <td style="width: 10%" @click="$emit('showmodify', transaction.id)"> {{ transaction.status }} </td>
         </tr>
     </table>
     `
 })
 
+Vue.component('recap', {
+    data: function () {
+        return {
+          sum: 0
+        }
+    },
+    methods: {
+        computeSum(transactions, users) {
+            sum = 0;
+            for (transaction of transactions) {
+                if (transaction.source == users.me.id) {
+                coeff = -1
+                } else if (transaction.destination == users.me.id) {
+                    coeff = 1
+                } else {
+                    return "???";
+                }
+                sum += transaction.amount * coeff
+            }
+            this.sum = sum
+            return sum
+        },
+        getSumStyle(sum) {
+            if (sum > 0) {
+                color =  "MediumSeaGreen";
+            } else if (sum < 0) {
+                color = "Tomato";
+            } else {
+                color = "Gray";
+            }
+            return {color: color};
+        },
+    },
+    props: ["transactions", "users"],
+    template: `
+    <table class="main_table" style="border: 1px solid #eeeeee">
+        <tr>
+            <td v-bind:style="getSumStyle(sum)"> {{ computeSum(transactions, users) }}</td>
+        </tr>
+    </table>
+    `
+})
 new Vue({
     delimiters: ['[[', ']]'],
     el: '#app',
