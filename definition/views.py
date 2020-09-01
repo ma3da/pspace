86e9d3540c6552aee54b0cdf72f4df290fe6f5ce
lpc <lpintocastaneda@gmail.com>
2020-09-02 00:27:52 +0200

    def_store_visited: preparing

--- definition/views.py
+++ definition/views.py
@@ -6,6 +6,7 @@ import bs4
 import re
 import operator
 
+
 def index(request):
     return render(request, "definition/index.html")
 
@@ -16,35 +17,49 @@ def check_input(word):
     return True
 
 
-def format_article(art):
-    return "</br>".join([str(div) for div in art])
+def format_articles(divs):
+    return "<br>".join([str(div) for div in divs])
+
+
+def get_articles_src_already_stored(word):
+    return None
 
 
 def get_definition_html(word):
+    """for the given word, return html code to embed."""
+
     word = word.lower()
     base_url = "https://www.cnrtl.fr/definition"
     url = f"https://www.cnrtl.fr/definition/{word}"
-    resp = requests.get(url)
-    soup = bs4.BeautifulSoup(resp.content, "html.parser")
-    links = [link for link in soup("a") if link.has_attr("onclick")]
-    re_num = re.compile(f"/definition/({word}//[0-9])")
-    suffixes = list(map(lambda x: x.group(1), 
-                filter(lambda x: x is not None,
-                map(re_num.search,
-                map(operator.itemgetter("onclick"), links)))))
-    def_html = []
-    for suffix in suffixes:
-        print("process", suffix)
-        url = f"{base_url}/{suffix}"
-        resp = requests.get(url)
-        soup = bs4.BeautifulSoup(resp.content, "html.parser")
-        arts = [div for div in soup("div")
-                if "id" in div.attrs and div["id"].startswith("art")]
-        if arts:
-            def_html.extend(arts[0])
-            def_html.append("<hr>")
+    articles_src = []
 
-    return format_article(def_html)
+    _stored = get_articles_src_already_stored(word)
+    if _stored is not None:
+        articles_src = _stored
+    else:
+        resp = requests.get(url)
+        html_content = resp.content
+        soup = bs4.BeautifulSoup(html_content, "html.parser")
+        links = [link for link in soup("a") if link.has_attr("onclick")]
+        re_num = re.compile(f"/definition/({word}//[0-9])")
+        for suffix in map(lambda x: x.group(1),
+                      filter(lambda x: x is not None,
+                      map(re_num.search,
+                      map(operator.itemgetter("onclick"), links)))):
+            url = f"{base_url}/{suffix}"
+            resp = requests.get(url)
+            articles_src.append(resp.content)
+
+    articles_divs = []
+    for html in articles_src:
+        soup = bs4.BeautifulSoup(html, "html.parser")
+        divss = [div for div in soup("div")
+                 if "id" in div.attrs and div["id"].startswith("art")]
+        if divss:
+            articles_divs.extend(divss[0])
+            articles_divs.append("<hr>")
+
+    return format_articles(articles_divs)
 
 
 class DefinitionView(APIView):
