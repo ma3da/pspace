1021e923ef0f3f3528d2f7b44099b7fd0dfd3962
lpc <lpintocastaneda@gmail.com>
2020-10-27 08:44:33 +0100

    new reqs + don't read uid for anonymous users

--- back/flaskr/views.py
+++ back/flaskr/views.py
@@ -11,14 +11,16 @@ def login():
     _user = dao_users.get_auth("0", data.get("pwd", None))
     if _user is not None:
         login_user(_user)
+    uid = None if current_user.is_anonymous else str(current_user.id)
     return jsonify({"logged": current_user.is_authenticated,
-                    "username": str(current_user.id)})
+                    "username": uid})
 
 
 @app.route("/logged", methods=["GET"])
 def logged():
+    uid = None if current_user.is_anonymous else str(current_user.id)
     return jsonify({"logged": current_user.is_authenticated,
-                    "username": str(current_user.id)})
+                    "username": uid})
 
 
 @app.route("/logout", methods=["POST"])
